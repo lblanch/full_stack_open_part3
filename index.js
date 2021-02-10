672503868925d9e669dad36112f1a1c23c684b84
Laura Blanch <laura.blancho@gmail.com>
2021-02-10 13:27:56 +0200

    3.3: Phonebook backend, step3

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const person = persons.find((p) => p.id === Number(request.params.id))
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get('/info', (request, response) => {
     response.send(`
         <div>
