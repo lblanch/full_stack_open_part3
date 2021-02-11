86758157483330f8d80ca2c5e057b0f4e7822189
Laura Blanch <laura.blancho@gmail.com>
2021-02-11 18:57:10 +0200

    3.11: Phonebook full stack, step11

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
